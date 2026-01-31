68c9d759db2f82ded9ebdf343ea9ae0d5b69da2f
GattamPavan <pavanreddy2266@gmail.com>
2026-01-31 16:08:52 +0530

    actionssss

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1="suresh" #no space allowed around =
+person2="ramesh"
+
+echo "$person1:: hello $person2"
+echo "$person2 :: im fine"
+echo "$person1 :: okay, what are you learning"
+echo "$person2 :: im learning shell" 

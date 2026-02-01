7ec2ea2a25bf4891597a0a64081740db05adae6b
GattamPavan <pavanreddy2266@gmail.com>
2026-02-01 13:00:58 +0530

    actions

---
+++ 09-conditions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+number=$1
+
+#-gt means greater than
+#-lt means less than
+#-eq means equal to
+#-ne means not equal to
+#-ge means greater than or equal to
+#-le means less than or equal to
+
+if [$number -gt 20]; then
+    echo "given number: $number is greater than 20"
+fi 
+ 

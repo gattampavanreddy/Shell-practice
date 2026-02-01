c8a74386bffce5acc500e03e5e205ea5255f7efc
GattamPavan <pavanreddy2266@gmail.com>
2026-02-01 12:19:38 +0530

    actions

--- 08-datatypes.sh
+++ 08-datatypes.sh
@@ -7,3 +7,13 @@ sum=$((num1+num2))
 
 echo "sum is: $sum"
 
+#array
+
+fruits=("apple" "banana" "grapes" "mango")
+
+echo "fruits are: ${fruits[@]}"
+echo "first fruit is: ${fruits[0]}"
+echo "second fruit is: ${fruits[1]}"
+echo "total number of fruits: ${#fruits[@]}"
+echo "total number of characters in first fruit: ${#fruits[2]}"
+

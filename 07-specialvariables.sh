dc4380be47943089ca55b4caed41cb50a956f33b
GattamPavan <pavanreddy2266@gmail.com>
2026-02-01 11:27:59 +0530

    actions

---
+++ 07-specialvariables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#### Special Variables in Bash ####
+
+echo "all arguments passed to script: $@"
+echo "number of variables passed: $#"
+echo "script name: $0"
+echo "present which directory you are in: $PWD"
+echo "who is runninf this script: $USER"
+echo "home directory of the user: $HOME"
+echo "pid of the current script: $$"
+sleep 100 &
+echo "pid of recently executed background process: $!"
+echo "all arguments passed to script: $*"
+

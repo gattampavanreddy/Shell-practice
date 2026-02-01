35fc6235d203696ecf628f2dfe575de998be4c66
GattamPavan <pavanreddy2266@gmail.com>
2026-02-01 10:32:13 +0530

    actions

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-person1="suresh" #no space allowed around =
-person2="ramesh"
+person1="modi" #no space allowed around =
+person2="putin"
 
 echo "$person1:: hello $person2"
 echo "$person2 :: im fine"
 echo "$person1 :: okay, what are you learning"
 echo "$person2 :: im learning shell" 
+
+# Variable names are case sensitive 
---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1 #no space allowed around =
+person2=$2
+
+echo "$person1:: hello $person2"
+echo "$person2 :: im fine"
+echo "$person1 :: okay, what are you learning"
+echo "$person2 :: im learning shell" 

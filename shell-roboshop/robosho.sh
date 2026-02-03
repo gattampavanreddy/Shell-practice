2fe521021e7c7e42ecad460fbc1eae16388859e9
GattamPavan <pavanreddy2266@gmail.com>
2026-02-03 21:35:09 +0530

    shell-roboshop

--- shell-roboshop/robosho.sh
+++ shell-roboshop/robosho.sh
@@ -5,9 +5,30 @@ AMI_ID= "ami-0220d79f3f480ecf5"
 
 for instance in $@
  do
-   aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro D --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'reservations[0].instances[0].
-   privateIpAddress' --output text
-done
+   InstanceId=$(aws ec2 run-instances \
+        --image-id $AMI_ID \
+        --instance-type "t3.micro" \
+        --security-group-ids $SG_ID \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+        --query 'Instances[0].InstanceId' \
+        --output text)
+
+        if [ $instance == "frontend"]; then
+          IP=$(
+            aws ec2 describe-instances \
+             --instance-ids $InstanceId \
+             --query 'Reservations[].Instances[].PublicIpAddress' \
+             --output text
+             )
+        else
+          IP=$(
+            aws ec2 describe-instances \
+             --instance-ids $InstanceId \
+             --query 'Reservations[].Instances[].PrivateIpAddress' \
+             --output text
+             )
+        fi
+    Done 
 
 
 

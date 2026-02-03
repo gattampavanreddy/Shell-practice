56459cb4ce21aa3156928b597b1c95c11cef4ef0
GattamPavan <pavanreddy2266@gmail.com>
2026-02-03 15:16:25 +0530

    shell install

---
+++ 12-logs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+userid=$(id -u)
+logfolder="/var/log/shell-script" 
+
+if [ $userid -ne 0 ]; then
+    echo 'please run this script as root user'
+    exit 1
+fi
+
+mkdir -p $logsfolder
+
+#by default shell will not execute, only executed when called
+validate(){
+       if [ $1 -ne 0 ]; then
+    echo "$2 failed"
+    exit 1
+else
+    echo "$2 successfully"
+fi 
+
+}
+
+dnf install nginx -y &>> $logsfolder
+validate $? "installing nginx"
+
+dnf install mysql -y &>> $logsfolder
+validate $? "installing mysql"
+
+dnf install nodejs -y &>> $logsfolder
+validate $? "installing nodejs"
